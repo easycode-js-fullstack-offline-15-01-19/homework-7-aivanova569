c9be4f149d54a7f7405108737889fc42fa7ae716
Anastasiia Ivanova <aivanova569@bigmir.net>
2019-02-16 16:45:57 +0200

    homework 7

--- homework.js
+++ homework.js
@@ -1,2 +1,51 @@
 // -------------------------- Home work --------------------------
-// -------------------------- ФИО --------------------------
+// ----------------------- Ivanova Anastasiia --------------------
+
+/* 1. Используя rest оператор и деструктуризацию, создать функцию,
+которая принимает любое количество аргументов и возвращает объект,
+содержащий первый аргумент и массив из остатка.
+*/
+/**
+ * Bозвращает объект, содержащий первый аргумент и массив из остатка
+ *
+ * @return {object} объект c первым аргументoм и массивoм из остатка.
+ */
+function getObj() {
+    const [first, ...other] = arguments;
+    return {first: first, other: other};
+}
+
+/* 2. Организовать функцию getInfo, которая принимает объект вида
+{ name: ...,  info: { employees: [...], partners: [ … ]  } }
+и выводит в консоль имя (если имени нет, показывать ‘Unknown’)
+ и первые две компании из массива partners
+ */
+const organisation = {  
+    name: 'Google',   
+    info: { employees: ['Vlad', 'Olga'], partners: ['Microsoft', 'Facebook', 'Xing'] } 
+};
+
+/**
+ * Bыводит в консоль имя и первые две компании из массива partners
+ *
+ * @param {object} объект.
+ */
+function getInfo(Obj) {
+    let {name = 'Unknown', info: {partners: [first, second]} } = Obj;
+    console.log(`Name: ${name}`);
+    console.log(`Partners: ${ [first, second] }`);
+}
+
+/* 3. Переделать функцию с использованием функции-стрелки
+(в методе reduce тоже использовать arrow function)
+*/
+/**
+ * Возвращает сумму элементов
+ *
+ * @return {number} результат вычисления.
+ */
+const sum = (...numbers) => {
+    const params = Array.prototype.slice.call(numbers);
+    if (!params.length) return 0;
+    return params.reduce( (prev, next) => prev + next);
+};
